b4f42f82b16c4c5a374296af81cf6d9a1ce717e6
Leonardo M. Ramé <martinrame@yahoo.com>
2015-11-28 08:18:04 -0300

    Se actualiza run.sh

--- soffice/run.sh
+++ soffice/run.sh
@@ -1,1 +1,8 @@
-soffice --invisible --accept="socket,host=127.0.0.1,port=2002,tcpNoDelay=1;urp;" --headless --nodefault --nofirststartwizard --nolockcheck --nologo --norestore 
+#!/bin/bash
+echo "Atención: este programa se ejecuta desde cron."
+
+pidof /usr/lib/libreoffice/program/soffice.bin  >/dev/null
+if [[ $? -ne 0 ]] ; then
+        echo "Restarting Soffice:     $(date)" >> /home/martin/soffice/restart.log
+        soffice --invisible --accept="socket,host=127.0.0.1,port=2002,tcpNoDelay=1;urp;" --headless --nodefault --nofirststartwizard --nolockcheck --nologo --norestore &
+fi
